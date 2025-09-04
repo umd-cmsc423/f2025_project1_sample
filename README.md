ae653cc164b70766a050c7607b5c3c739ec0576b
Rob Patro <rob.patro@gmail.com>
2025-09-03 21:55:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMSC 423: Fall 2024, Project 1 Sample
+
+A sample skeleton for Project 1 (in Java)
---
+++ src/FastaStats.java
@@ -0,0 +1,7 @@
+public class FastaStats {
+        public static void main(String[] args) {
+          String fname = args[0];
+
+          System.out.format("input file: %s\n", fname);
+        }
+}
